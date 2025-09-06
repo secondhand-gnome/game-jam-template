47715546a7850bc4080e51d4c21cc2658e43090b
Austin Almond <aust.almond@gmail.com>
2025-09-06 11:53:56 -0400

    wip control map

--- game-jam-template/ui/options/controls/control_row.gd
+++ game-jam-template/ui/options/controls/control_row.gd
@@ -1,21 +1,35 @@
 @tool
-extends HBoxContainer
+extends Button
 
 @export var guide_action: GUIDEAction
 
-@onready var _name_label: Label = $ControlLabel
-@onready var _icon_label: RichTextLabel = $ControlIcon
+@onready var _is_remapping: bool = false
 
 @onready var _formatter: GUIDEInputFormatter = \
 	GUIDEInputFormatter.for_active_contexts()
 
 func _ready():
-	_name_label.text = guide_action.display_name
+	text = guide_action.display_name
 
 	if not Engine.is_editor_hint():
 		GUIDE.input_mappings_changed.connect(_on_input_mappings_changed)
 		_on_input_mappings_changed()
 
+func _input(event):
+	if not _is_remapping:
+		return
+
+	# TODO filter only to valid types of input
+	if event.is_pressed():
+		print("Mapping %s to %s" % [guide_action.display_name, event])
+		_is_remapping = false
+
 func _on_input_mappings_changed():
-	var action_text := await _formatter.action_as_richtext_async(guide_action)
-	_icon_label.text = action_text
+	var action_text := _formatter.action_as_text(guide_action)
+	text = "%s - %s" % [guide_action.display_name, action_text]
+
+func _on_pressed():
+	if _is_remapping:
+		return
+	print("Remapping input %s" % guide_action.display_name)
+	_is_remapping = true
